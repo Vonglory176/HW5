d806a471b27c1238f57beb5e716994716d0306a8
Vonglory176 <SkylerGConley@gmail.com>
2023-02-03 21:09:49 -0800

    Overhauled JS and added more jQuery, creating task completion code

--- main.js
+++ main.js
@@ -1,106 +1,126 @@
-document.addEventListener("DOMContentLoaded", function (_) {
-    document.getElementById('addItemButton').addEventListener('click', addItem)
-    document.getElementById('viewMyTasksButton').addEventListener('click', viewMyTasks)
-    document.getElementById('viewHighPriorityOnlyButton').addEventListener('click', showHighPriorityTasks)
-    document.getElementById('prioritizeButton').addEventListener('click', prioritizeTasks())
-
+$(document).ready(function () {
+    $('#addItemButton').on('click', addItem)
+    $('#viewMyTasksButton').on('click', viewMyTasks)
+    $('#viewHighPriorityOnlyButton').on('click', showHighPriorityTasks)
+    //$('#prioritizeButton').on('click', prioritizeTasks())
+    $('#viewMyCompletedTasksButton').on('click', printCompletedTasks)
 });
 
-
-//ARRAY HOLDING TO-DO LIST ITEMS
-const toDoArray = []
-
-
-//TO-DO LIST OBJECT CREATION
-function task(task, priority) {
-    this.task = task
-    this.priority = priority
-}
+//TO-DO LIST OBJECT and ARRAY HOLDING TO-DO LIST ITEM CREATION
+function task(task, priority) {this.task = task, this.priority = priority}
+let toDoArray = []
 
 //FUNCTION ADDING ITEMS TO TO-DO LIST
 async function addItem() {
     try {
         //GRABBING INFO FROM URGENCY SELECTOR RADIO BUTTONS
-        let radio = await radiocheck()
-        let todo = document.getElementById('toDoInput').value
-        let urgency
+        let radioID = await radiocheck(), todo = await inputCheck() //Verifying Button-Press/Text-Input
+        let priority
 
         //TO-DO LIST ITEM CREATION & ARRAY PUSH
-        radio === 'radioVeryHigh' ? (urgency = 'very high') :
-            radio === 'radioHigh' ? (urgency = 'high') :
-                radio === 'radioMedium' ? (urgency = 'medium') :
-                    (urgency = 'low')
+        radioID === 'radioVeryHigh' ? (priority = 1) :
+            radioID === 'radioHigh' ? (priority = 2) :
+                radioID === 'radioMedium' ? (priority = 3) :
+                    (priority = 4)
 
-
-        toDoArray.push(new task(todo, urgency))
-        console.log(toDoArray) //FOR DEBUGGING
+        toDoArray.push(new task(todo, priority))
 
         //RADIO & TEXT BOX RESET
-        //this isn't working 
-        //document.getElementByID(radio).checked = false
-        document.getElementById("toDoInput").value = "";
+        $(`#${radioID}`).attr('checked', false)
+        $("#toDoInput").val('')
 
-    } catch { alert("Unable to add item to list") }
-
+    } catch (error) { alert(error) }
 }
 
-//RADIO BUTTON CHECK
+//Radio Button Check
 function radiocheck() {
     return new Promise((resolve, reject) => {
         document.querySelectorAll('input[name="urgencySelector"]').forEach(i => { //Iterating all input elements with name 'urgencySelector'
             if (i.checked) resolve(i.id) //Returns ID of checked
-            console.log(i.checked, i.id) //FOR DEBUGGING
         })
-        reject("Please select an urgency level for the task")  //Returns error if none
+        reject("Please select an priority level!")
     })
 }
 
+//Input Check
+function inputCheck() {
+    return new Promise((resolve, reject) => {
+        input = ($('#toDoInput').val()).trim()
+        input? resolve(input) : reject("Please enter a valid task!") //Verifying input isn't just whitespace
+    })
+}
 
 //LIST TASKS IN PROGRESS
 //SHOW TO-DO LIST BUTTON CLICKED
 function viewMyTasks() {
-    let theList = document.getElementById("inProgressTasksDisplay");
-    theList.innerHTML = " ";
-    toDoArray.forEach(function (element) {
-        var list = document.createElement('li');
-        list.innerHTML = element.task;
-
-        //COLOR CODE THE ITEM BASED ON PRIORITY LEVEL
-        if (element.priority === 'very high') {
-            list.style.color = 'rgb(212, 2, 2)';
-        } else if (element.priority === 'high') {
-            list.style.color = 'rgb(255, 68, 0)';
-        } else if (element.priority === 'medium') {
-            list.style.color = 'rgb(239, 168, 3)';
-        } else {
-            list.style.color = 'green';
-        }
-
-        theList.appendChild(list);
-    })
+    $("#inProgressTasksDisplay").html("") //Resetting Current Listed Tasks
+    let toDoArray = sortArray() //Sorting array from highest to low priority 
+    
+    for (let i = 0; i < toDoArray.length; i++) {
+        //Verifying task is incomplete before printing
+        if(toDoArray[i].priority !== 5) printUncompletedTasks(toDoArray[i].task, i, toDoArray[i].priority)//Text, Index, Priority
+    }
 }
 
 //VIEW HIGH PRIORITY TASKS ONLY
 function showHighPriorityTasks() {
-    let theList = document.getElementById("inProgressTasksDisplay");
-    theList.innerHTML = " ";
-    toDoArray.forEach(function (element) {
-        if (element.priority === 'very high') {
-            var list = document.createElement('li');
-            list.style.color = 'rgb(212, 2, 2)';
-            list.innerHTML = element.task + " -- " + element.priority + " priority";
-            theList.appendChild(list);
+    $("#inProgressTasksDisplay").html("") //Resetting Current Listed Tasks
+    let toDoArray = sortArray()  //Sorting array from highest to low priority 
+
+    for(let i = 0; i < toDoArray.length; i++){
+        if (toDoArray[i].priority === 1 || toDoArray[i].priority === 2){
+
+            let elementText = toDoArray[i].task + " -- " + (toDoArray[i].priority === 1 ?'Very high':'High') + " priority"
+            printUncompletedTasks(elementText, i, toDoArray[i].priority)//Text, Index, Priority
+            
         }
-    })
-    toDoArray.forEach(function (element) {
-        if (element.priority === 'high') {
-            var list = document.createElement('li');
-            list.style.color = 'rgb(255, 68, 0)';
-            list.innerHTML = element.task + " -- " + element.priority + " priority";
-            theList.appendChild(list);
-        }
-    })
+    }
 }
+
+//Array Sorter
+function sortArray(){ 
+    return toDoArray.sort((p1, p2) => (p1.priority > p2.priority) ? 1 : (p1.priority < p2.priority) ? -1 : 0)
+}
+
+//Uncompleted Task printing
+function printUncompletedTasks(text, index, priority){
+    //Radio Button
+    let elementRadio = $(`<input type='radio'>`)//Setting HTML
+    $(elementRadio).attr('id', `taskRadio${index}`)//Setting ID
+
+    $(elementRadio).on('click', function(){//Attaching Event Handler
+        toDoArray[index].priority = 5
+        $(`#taskLabel${index}`).css('color',colorPicker(5))
+    })
+
+    //Affiliated Label
+    let elementLabel = $(`<label>Completed? &emsp; ${text}</label> <br>`)//Setting HTML & Text
+    $(elementLabel).attr('for', `taskRadio${index}`)//Linking to Radio Button
+    $(elementLabel).attr('id', `taskLabel${index}`)//Setting ID
+    $(elementLabel).css('color',`${colorPicker(priority)}`)//Setting Color
+
+    $("#inProgressTasksDisplay").append($(elementRadio),$(elementLabel))//Printing to Screen
+}
+
+//Color Code Based on Priority System
+function colorPicker(priority) {
+    if      (priority === 1) return 'rgb(212, 2, 2)'
+    else if (priority === 2) return 'rgb(255, 68, 0)'
+    else if (priority === 3) return 'rgb(239, 168, 3)'
+    else if (priority === 4) return 'green'
+    else if (priority === 5) return 'darkgreen' //For Complete
+}
+
 
 //PRIORITIZE MY TASKS BUTTON CLICK
 
+//VIEW COMPLETED TASKS BUTTON CLICK
+function printCompletedTasks() {
+    for (let i = 0; i < toDoArray.length; i++){
+        if (toDoArray[i].priority === 5) {
+            let element = $(`<li>${toDoArray[i].task}</li>`)//Setting HTML
+            $(element).css('color',`${colorPicker(toDoArray[i].priority)}`)//Setting Color
+            $("#tasksCompleted").append($(element))//Printing to Screen
+        }
+    }
+}
